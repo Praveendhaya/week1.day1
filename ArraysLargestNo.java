a623258fd8ba8bd78a87ee95d3a5c27622474e62
Praveen <64059123+Praveendhaya@users.noreply.github.com>
2022-05-19 17:52:55 +0530

    Add files via upload

---
+++ ArraysLargestNo.java
@@ -0,0 +1,14 @@
+package Assignment2;
+
+import java.util.Arrays;
+
+public class ArraysLargestNo {
+	public static void main(String[] args) {
+
+		  int[] data = {3,2,11,4,6,7};
+		  Arrays.sort(data);
+		  int length = data.length;
+		  System.out.println(data[length-2]);
+	}
+
+}
